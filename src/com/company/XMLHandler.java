fbd80caf3b8630a6419269d27589b0e9fc5d459b
ivanyaramov <ivan.yaramov@gmail.com>
2022-05-10 00:14:11 +0300

    adding comments

--- src/com/company/XMLHandler.java
+++ src/com/company/XMLHandler.java
@@ -4,7 +4,9 @@ import com.company.exceptions.*;
 
 import java.util.*;
 
+//клас който манипулира информацията от xml файла
 public class XMLHandler {
+    //метод, който превръща низа, прочетен от файла, в java обекти
     public static XMLRepresentation convertStringToXMLObjects(String content) {
         StartIndexKeeper startIndexKeeper = new StartIndexKeeper(0);
         ArrayDeque<XMLElement> stack = new ArrayDeque<>();
@@ -47,6 +49,7 @@ public class XMLHandler {
         return xmlRepresentation;
     }
 
+    //метод, който парсира обратно обектите към текст
     public static String convertXMLObjectsToString(XMLRepresentation xmlRepresentation) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < xmlRepresentation.getListOfElements().size(); i++) {
@@ -55,6 +58,7 @@ public class XMLHandler {
         return sb.toString();
     }
 
+    //слага недублиращи ид-та на елементите, които имат дублиращи ид-та и слага ид-та на тези, които нямат
     public static void setUnDublicatingIdsToElements(XMLRepresentation xmlRepresentation) {
         Set<String> duplicatedElements = new HashSet<>();
         Set<String> allIds = new HashSet<>();
@@ -62,12 +66,14 @@ public class XMLHandler {
         setIdsOfElementsInRepresentation(duplicatedElements, xmlRepresentation, allIds);
     }
 
+    //намира дубликиращите се ид-та в цялата структура
     public static void findDuplicatingIds(XMLRepresentation xmlRepresentation, Set<String> duplicatedElements, Set<String> allElements) {
         for (int i = 0; i < xmlRepresentation.getListOfElements().size(); i++) {
             XMLHandler.findDuplicatingIdsInChildElements(xmlRepresentation.getListOfElements().get(i), duplicatedElements, allElements);
         }
     }
 
+    //добавя ид-то на подадения елемент в подадената колекция, ако той се дублира и обхожда неговите деца
     public static void findDuplicatingIdsInChildElements(XMLElement xmlElement, Set<String> duplicatedElements, Set<String> allElements) {
 
         if (xmlElement.getAttributes() != null) {
@@ -88,6 +94,7 @@ public class XMLHandler {
         }
     }
 
+    //слага не-дубликиращи ид-та във всички елементи на репрезентацията
     public static void setIdsOfElementsInRepresentation(Set<String> duplicatedElements, XMLRepresentation xmlRepresentation, Set<String> allIds) {
         Map<String, String> duplicatedElementsCounter = new HashMap<>();
         for (int i = 0; i < xmlRepresentation.getListOfElements().size(); i++) {
@@ -95,6 +102,8 @@ public class XMLHandler {
         }
     }
 
+    //метод, съдържащ логиката на това да се слагат уникално ид на подадения елемент, като се гледа дали се съдържа в
+    //колекцията с дубликиращи се ид-та, а самото ид се избира като се гледа да не се съдържа в колекцията с всички текущи ид-та
     public static void setIdsOfElements(Set<String> duplicatedElements, Set<String> allIds, XMLElement xmlElement, Map<String, String> duplicatedElementsCounter) {
         if (xmlElement.getAttributes() != null) {
             Map<String, String> map = xmlElement.getAttributes();
@@ -126,6 +135,7 @@ public class XMLHandler {
         }
     }
 
+    //метод който избира уникално ид, като инкрементира от стойност 1, докато не намери такова каквото не се съдържа в колекцията с всички ид-та
     private static void setUntakenID(Map<String, String> map, Set<String> allIds, XMLElement xmlElement) {
         String id = "1";
         while (allIds.contains(id)) {
@@ -138,6 +148,7 @@ public class XMLHandler {
         allIds.add(id);
     }
 
+    //слагане на нов атрибут/ модифициране на стар атрибут по ключ на подаден елемент
     public static void setAttributeValueById(String id, String key, String value, XMLRepresentation xmlRepresentation) throws IdNotFoundException {
         XMLElement element = XMLHandler.findElementById(id, xmlRepresentation);
         element.getAttributes().put(key, value);
@@ -145,6 +156,7 @@ public class XMLHandler {
 
     }
 
+    //връща стойността на атрибут, като е зададен ключа му и елемента
     public static String getAttributeValueByElementId(String id, String key, XMLRepresentation xmlRepresentation) throws IdNotFoundException, AttributeNotFoundException {
         XMLElement element = XMLHandler.findElementById(id, xmlRepresentation);
         if (!element.getAttributes().containsKey(key)) {
@@ -153,6 +165,7 @@ public class XMLHandler {
         return element.getAttributes().get(key);
     }
 
+    //търсене на елемент по ид в репрезентацията
     public static XMLElement findElementById(String id, XMLRepresentation xmlRepresentation) throws IdNotFoundException {
         for (XMLElement el : xmlRepresentation.getListOfElements()) {
             XMLElement newEl = XMLHandler.findElementByIdInElement(el, id);
@@ -163,6 +176,7 @@ public class XMLHandler {
         throw new IdNotFoundException("There is no element with id " + id);
     }
 
+    //намиране на елемент по ид в подаден елемент и децата му
     public static XMLElement findElementByIdInElement(XMLElement xmlElement, String id) {
         if (xmlElement.getAttributes().get("id").equals(id)) {
             return xmlElement;
@@ -176,6 +190,7 @@ public class XMLHandler {
         return null;
     }
 
+    //намиране на елемент по име в подаден елемент
     public static void findElementsByNameInElement(String parentName, String expectedParentName, XMLElement xmlElement, String name, List<XMLElement> list) {
         if (expectedParentName != null) {
             if (xmlElement.getName().equals(name) && parentName.equals(expectedParentName)) {
@@ -189,6 +204,7 @@ public class XMLHandler {
         }
     }
 
+    //намиране на елемент по име в цялата репрезентация
     public static List<XMLElement> findElementsByName(String expectedParentName, String name, XMLRepresentation xmlRepresentation) {
         List<XMLElement> list = new ArrayList<>();
         for (XMLElement el : xmlRepresentation.getListOfElements()) {
@@ -197,6 +213,7 @@ public class XMLHandler {
         return list;
     }
 
+    //намрине на елемент, ако някое от децата му има подадената стойност (в цялата репрезентация)
     public static List<XMLElement> findElementsByChildrenValue(String parentName, String childName, String value, String get, XMLRepresentation xmlRepresentation) {
         List<XMLElement> list = new ArrayList<>();
         for (XMLElement el : xmlRepresentation.getListOfElements()) {
@@ -205,6 +222,8 @@ public class XMLHandler {
         return list;
     }
 
+    //намиране на елемент по стойност на някое от децата му има подадената стойност
+    //ако отговаря на това условие се добавя в списък
     public static void findElementsByChildrenValueInElements(String parentName, String childName, String value, String get, List<XMLElement> list, XMLElement xmlElement) {
         boolean toAdd = false;
         if (xmlElement.getName().equals(parentName)) {
@@ -228,7 +247,7 @@ public class XMLHandler {
         }
     }
 
-
+    //намиране на всички атрибути на децата на елемент в цялата, като му е подадено ид-то му
     public static String getAttributesOfChildren(String id, XMLRepresentation xmlRepresentation) throws IdNotFoundException {
         StringBuilder sb = new StringBuilder();
         XMLElement element = XMLHandler.findElementById(id, xmlRepresentation);
@@ -240,6 +259,7 @@ public class XMLHandler {
         return sb.toString();
     }
 
+    //намиране на всички атрибути на децата на подаден елемент и се добавят в StringBuilder
     public static void getAttributesOfChildrenAndCurrent(XMLElement element, StringBuilder sb) {
         if (element.getAttributes() != null) {
             StringHandler.addAttributesOfElementToStringBuilder(element.getAttributes(), sb);
@@ -251,6 +271,7 @@ public class XMLHandler {
         }
     }
 
+    //намиране на N-то дете на елемент, като му е подадено ид-то
     public static String getNthChildOfElement(String id, int n, XMLRepresentation xmlRepresentation) throws IdNotFoundException, ChildNotFoundException {
         XMLElement xmlElement = XMLHandler.findElementById(id, xmlRepresentation);
         if (xmlElement.getChildren().size() < n - 1) {
@@ -261,6 +282,7 @@ public class XMLHandler {
         return sb.toString();
     }
 
+    //намиране на текста на елемент, като му е подадено ид-то (обхваща и текста на децата му)
     public static String getTextOfElement(String id, XMLRepresentation xmlRepresentation) throws IdNotFoundException {
         XMLElement xmlElement = XMLHandler.findElementById(id, xmlRepresentation);
         StringBuilder sb = new StringBuilder();
@@ -268,6 +290,7 @@ public class XMLHandler {
         return sb.toString();
     }
 
+    //трие атрибут на елемент като е подаден ключа на атрибута и ид-то на елемента
     public static void deleteElementAttributeByKey(String id, String key, XMLRepresentation xmlRepresentation) throws IdNotFoundException {
         XMLElement xmlElement = XMLHandler.findElementById(id, xmlRepresentation);
         if (xmlElement.getAttributes() != null) {
@@ -275,6 +298,8 @@ public class XMLHandler {
         }
     }
 
+    //добавяне на нов елемент с подадено име към съществуващ елемент с подадено ид
+    //ид-тата се проверяват и се слагат наново, за да е сигурно че няма дубликати
     public static void addElementToElement(String id, String name, XMLRepresentation xmlRepresentation) throws IdNotFoundException, CannotAddChildException {
         XMLElement xmlElement = XMLHandler.findElementById(id, xmlRepresentation);
         if (xmlElement.getValue() != null) {
@@ -285,10 +310,12 @@ public class XMLHandler {
         XMLHandler.setUnDublicatingIdsToElements(xmlRepresentation);
     }
 
+    //намира всички елементи по подадено име, в бащин елемент чието име също е подадено
     public static List<XMLElement> xPathElementList(String elementName, String expectedParentName, XMLRepresentation xmlRepresentation) {
         return XMLHandler.findElementsByName(expectedParentName, elementName, xmlRepresentation);
     }
 
+    //като горната функция, само че връща само 1 елемент с индекс подадения
     public static XMLElement xPathElement(String expectedParentName, String elementName, XMLRepresentation xmlRepresentation, int number) throws ElementNotFoundException {
         List<XMLElement> list = XMLHandler.findElementsByName(expectedParentName, elementName, xmlRepresentation);
         if (number > list.size() - 1) {
@@ -297,6 +324,7 @@ public class XMLHandler {
         return list.get(number);
     }
 
+    //връща списък с всички ид-та на децата на елемент, чието име е подадено
     public static List<String> xPathGetAllIdsOfElement(String elementName, XMLRepresentation xmlRepresentation) throws ElementNotFoundException {
         List<XMLElement> xmlElements = XMLHandler.findElementsByName(null, elementName, xmlRepresentation);
         List<String> listOfIds = new ArrayList<>();
@@ -309,6 +337,7 @@ public class XMLHandler {
         return listOfIds;
     }
 
+    //връща всички елементи с подаденото име, които отговарят на подаденото име и съдържат в себе си дете с подаденото име и подадената стойност
     public static List<XMLElement> xPathFindElementsByNameAndValueOfChild(String elementName, String parentName, String value, String get, XMLRepresentation xmlRepresentation) throws ElementNotFoundException {
         List<XMLElement> list = XMLHandler.findElementsByChildrenValue(parentName, elementName, value, get, xmlRepresentation);
         if (list.size() == 0) {
